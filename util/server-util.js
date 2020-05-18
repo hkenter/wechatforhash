9c66d8d70f2ac582ad331d9fea3cc359cb4c2bd7
Kelly Cheng <wt696969@126.com>
2020-05-18 13:28:52 +0800

    add server-util

---
+++ util/server-util.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const swig = require('swig');
+
+app = express();
+//设置渲染文件的目录
+app.set('views','/views');
+//设置html模板渲染引擎
+app.engine('html',swig.renderFile);
+//设置渲染引擎为html
+app.set('view engine','html');
+
+app.listen(9527);
+
+//调用路由，进行页面渲染
+app.get('/overview',function(request,response){
+    //调用渲染模板
+    response.render('overview',{
+        //传参
+        code:'600208'
+    });
+
+});
