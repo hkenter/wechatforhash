945b597acc5c4bd9d17a6a71d2e6fb50df414c93
Kelly Cheng <wt696969@126.com>
2020-05-19 10:41:30 +0800

    fix bug

--- util/puppeteer-util.js
+++ util/puppeteer-util.js
@@ -1,7 +1,13 @@
 const puppeteer = require('puppeteer');
 
 async function getOverviewScreenshot(ticker) {
-    const browser = await getBrowser();
+    const browser = await puppeteer.launch({
+        // slowMo: 100,    //放慢速度
+        headless: false,
+        // defaultViewport: {width: 1440, height: 780},
+        ignoreHTTPSErrors: false, //忽略 https 报错
+        args: ['--no-sandbox', '--lang=zh_CN.UTF-8', '--disable-gpu'] // linux root 必填
+    });
     const page = await browser.newPage();
     // await page.goto('http://' + 'localhost:9527' + '/' + func + `?ticker=${ticker}`);
     await page.goto(`https://s.tradingview.com/mediumwidgetembed/?symbols=SSE%3A${ticker}%7C1d&locale=zh_CN&trendLineColor=%232196F3&underLineColor=%23E3F2FD&fontColor=%23787B86&gridLineColor=%23F0F3FA&width=1000px&height=calc(400px%20-%2032px)&colorTheme=light&utm_source=localhost&utm_medium=widget_new&utm_campaign=symbol-overview`);
