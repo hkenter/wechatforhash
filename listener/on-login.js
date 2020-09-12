8917df7120b8a7a4543526ee5b30486334b2fc16
hkenter <wt696969@126.com>
2020-09-12 20:17:10 +0800

    add news

--- listener/on-login.js
+++ listener/on-login.js
@@ -11,37 +11,37 @@ async function onLogin (user) {
     console.log(`${user}登录了`);
 
     //每分钟的第8秒定时执行一次:
-    const dt_room = await bot.Room.find({topic: '数字部落★提升群'}); //嘎嘎嘎\数字部落★提升群
-    let json_basic_news = await RestUtil.getNews();
-    await schedule.scheduleJob('8 * * * * *',async ()=>{
-        await console.log('scheduleCronstyle:' + new Date());
-        let json_news = await RestUtil.getNews();
-
-        let titles = new Map();
-        for(let json_new of json_news['results']) {
-            titles.set(json_new['title'], true);
-        }
-        console.log(titles);
-        for(let json_basic_new of json_basic_news['results']) {
-            for (let [key, value] of titles) {
-                if (json_basic_new['title'] == key) {
-                    titles.set(key, false);
-                }
-            }
-        }
-        console.log(titles);
-        for (let [key, value] of titles) {
-            if (value) {
-                translate(key, {to: 'zh-CN'}).then(res => {
-                    console.log(res.text);
-                    delay.execute(() => dt_room.say(key + "\r\n" + res.text));
-                }).catch(err => {
-                    console.error(err);
-                });
-            }
-        }
-        json_basic_news = json_news;
-    });
+    // const dt_room = await bot.Room.find({topic: '数字部落★提升群'}); //嘎嘎嘎\数字部落★提升群
+    // let json_basic_news = await RestUtil.getNews();
+    // await schedule.scheduleJob('8 * * * * *',async ()=>{
+    //     await console.log('scheduleCronstyle:' + new Date());
+    //     let json_news = await RestUtil.getNews();
+    //
+    //     let titles = new Map();
+    //     for(let json_new of json_news['results']) {
+    //         titles.set(json_new['title'], true);
+    //     }
+    //     console.log(titles);
+    //     for(let json_basic_new of json_basic_news['results']) {
+    //         for (let [key, value] of titles) {
+    //             if (json_basic_new['title'] == key) {
+    //                 titles.set(key, false);
+    //             }
+    //         }
+    //     }
+    //     console.log(titles);
+    //     for (let [key, value] of titles) {
+    //         if (value) {
+    //             translate(key, {to: 'zh-CN'}).then(res => {
+    //                 console.log(res.text);
+    //                 delay.execute(() => dt_room.say(key + "\r\n" + res.text));
+    //             }).catch(err => {
+    //                 console.error(err);
+    //             });
+    //         }
+    //     }
+    //     json_basic_news = json_news;
+    // });
 }
 
 module.exports = onLogin;
