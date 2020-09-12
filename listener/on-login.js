c331bc59940f0a7101b19ee8aa92ca2e084cda65
hkenter <wt696969@126.com>
2020-09-12 16:09:28 +0800

    add news

--- listener/on-login.js
+++ listener/on-login.js
@@ -3,6 +3,7 @@ const RestUtil = require('./../util/rest-util');
 const schedule = require('node-schedule');
 const DelayQueueExecutor = require('rx-queue').DelayQueueExecutor;
 const delay = new DelayQueueExecutor(5 * 1000);
+const translate = require('google-translate-api');
 
 // 登录
 async function onLogin (user) {
@@ -10,18 +11,33 @@ async function onLogin (user) {
     console.log(`${user}登录了`);
 
     //每分钟的第8秒定时执行一次:
-    const dt_room = await bot.Room.find({topic: '数字部落★提升群'});
+    const dt_room = await bot.Room.find({topic: '嘎嘎嘎'}); //嘎嘎嘎\数字部落★提升群
     let json_basic_news = await RestUtil.getNews();
     await schedule.scheduleJob('8 * * * * *',async ()=>{
         await console.log('scheduleCronstyle:' + new Date());
         let json_news = await RestUtil.getNews();
 
+        let titles = new Map();
         for(let json_new of json_news['results']) {
-            console.log(json_new);
-            for(let json_basic_new of json_basic_news['results']) {
-                if (json_new['title'] !== json_basic_new['title']) {
-                    await delay.execute(() => dt_room.say(json_new['title']));
+            titles.set(json_new['title'], true);
+        }
+        console.log(titles);
+        for(let json_basic_new of json_basic_news['results']) {
+            for (let [key, value] of titles) {
+                if (json_basic_new['title'] == key) {
+                    titles.set(key, false);
                 }
+            }
+        }
+        console.log(titles);
+        for (let [key, value] of titles) {
+            if (value) {
+                translate(key, {to: 'zh-CN'}).then(res => {
+                    console.log(res.text);
+                    delay.execute(() => dt_room.say(res.text));
+                }).catch(err => {
+                    console.error(err);
+                });
             }
         }
         json_basic_news = json_news;
